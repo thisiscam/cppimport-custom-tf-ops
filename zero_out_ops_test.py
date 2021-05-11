064c5089a62589c267a45a8f1f8af8dae2fce4b8
Cambridge Yang <jq_yang@mit.edu>
2021-05-10 23:41:13 -0400

    fix indentation

--- zero_out_ops_test.py
+++ zero_out_ops_test.py
@@ -1,10 +1,10 @@
 def load_ops(ops_name: str):
-	import cppimport
-	from tensorflow.python.framework import load_library
-	from tensorflow.python.platform import resource_loader
+  import cppimport
+  from tensorflow.python.framework import load_library
+  from tensorflow.python.platform import resource_loader
 
-	shared_lib_path = cppimport.build(ops_name)
-	return load_library.load_op_library(resource_loader.get_path_to_datafile(shared_lib_path))
+  shared_lib_path = cppimport.build(ops_name)
+  return load_library.load_op_library(resource_loader.get_path_to_datafile(shared_lib_path))
 
 
 zero_out_ops = load_ops("zero_out_ops")
